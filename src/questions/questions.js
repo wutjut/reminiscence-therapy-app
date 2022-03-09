b4605dbffd90649c477a9ce17344c3d80b4aeb3b
restrepobs <brian@viulogix.com>
2022-03-09 15:32:29 -0500

    created card stack with swiping

---
+++ src/questions/questions.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        text: "What places can you travel to over and over again?",
+    },
+    {
+        text: "What famous or important people have you encountered in real life?",
+    },
+    {
+        text: "Has your relationship with your siblings changed over the years?",
+    },
+    {
+        text: "Tell me what a typical day in the military was like for you.",
+    },
+    {
+        text: "How has your life turned out differently than you imagined it would?",
+    },
+    {
+        text: "Which family members do you wish you kept better in touch with?",
+    },
+    {
+        text: "What is one of your favorite memories of your mother?",
+    },
+    {
+        text: "What games did you play when you were young?",
+    },
+    {
+        text: "Are you the same person you were as an adolescent, or very different?",
+    },
+    {
+        text: "What was your best boss like?",
+    },
+    {
+        text: "Do you believe that people can change? Why or why not?",
+    }
+
+
+]
--- src/screens/Questions.js
+++ src/screens/Questions.js
@@ -1,14 +1,61 @@
 // Aboutscreen.js
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Swiper from 'react-native-deck-swiper';
+import questions from '../questions/questions';
+
+const Card = ({card}) => (
+  <View style={styles.card}>
+     <Text>{card.text}</Text>
+    </View>
+)
 
 export default class QuestionsScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      index: 0,
+    }
+  }
+  onSwiped = () => {
+    this.setState({index: this.state.index+1});
+  }
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Questions</Text>
+      <View style={styles.container}>
+        <Swiper
+          cards={questions}
+          cardIndex={this.state.index}
+          renderCard={(card) => <Card card={card}/>}
+          onSwiped={this.onSwiped}
+          stackSize={4}
+          stackScale={10}
+          stackSeparation={14}
+          disableTopSwipe
+          disableBottomSwipe
+          disableRightSwipe
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center,'
+  },
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: '#000',
+    shadowOpacity: 0.08,
+    shadowOffset: {width: 0, height: 0},
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  }
+})
