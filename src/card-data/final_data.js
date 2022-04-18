510355163cd12e96454ca8cd14738c0980e007bc
aecozart <aecozart@gmail.com>
2022-04-18 14:40:47 -0400

    Json file added

---
+++ src/card-data/final_data.js
@@ -0,0 +1,482 @@
+export default [
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia600606.us.archive.org/20/items/78_over-the-rainbow_judy-garland-victor-young-and-his-orchestra-harold-arlen-e.-y.-har_gbia0004863a/Over%20The%20Rainbow%20-%20Judy%20Garland%20-%20Victor%20Young%20and%20his%20Orchestra-restored.flac",
+        answer: "Over the Rainbow",
+        decade: "30"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://www.whizzpast.com/wp-content/uploads/2015/02/best1930spics.jpg",
+        audio: "",
+        answer: "Prohibition",
+        decade: "30"
+    },
+    {
+        text: "Economic downturn that began in 1929 and ended around 1939",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "The Great Depression",
+        decade: "30"
+    },
+    {
+        text: "During the 1930s who wrote a syndicated newspaper column titled 'My Day'?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Eleanor Roosevelt",
+        decade: "30"
+    },
+    {
+        text: "Which award(s) was/were given for the first time in the 1930s?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "The Heisman Trophy",
+        decade: "30"
+    },
+    {
+        text: "Which of the following acclaimed horses was the only one to win the Kentucky Derby in the 1930s?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "War Admiral",
+        decade: "30"
+    },
+    {
+        text: "Which famous movie actress's father was the Speaker of the United States House of Representative from 1936-1940?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Tallulah Bankhead",
+        decade: "30"
+    },
+    {
+        text: "Who was the only future US president born in the 1930s?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "None",
+        decade: "30"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://imgix.ranker.com/user_node_img/68/1352970/original/katharine-hepburn-theater-actors-photo-u29?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=650",
+        audio: "",
+        answer: "Actress Katharine Hepburn",
+        decade: "30"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia803104.us.archive.org/9/items/cd_heartbreak-hotel_elvis-presley/disc1/01.%20Elvis%20Presley%20-%20Heartbreak%20Hotel_sample.mp3",
+        answer: "Whistle While You Work by Adriana Caselotti",
+        decade: "30"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia902807.us.archive.org/34/items/78_choreography_danny-kaye-the-skylarks-irving-berlin-joseph-j-lilley_gbia0071173/10%20-%20White%20Christmas%20%28Finale%29%20-%20Bing%20Crosby%20-%20Danny%20Kaye-restored.flac",
+        answer: "White Christmas",
+        decade: "40"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://www.archives.gov/files/exhibits/powers-of-persuasion/images/13229_2011_001_AC.jpg",
+        audio: "",
+        answer: "World War II Propoganda",
+        decade: "40"
+    },
+    {
+        text: "June 6, 1944, Allied forces launch an attack on Nazi-occupied France",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "D-Day",
+        decade: "40"
+    },
+    {
+        text: "How much was the first Polaroid Camera sold for in 1949?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "$89.95",
+        decade: "40"
+    },
+    {
+        text: "In 1946, the Philippines gained independence after how many years of U.S. rule?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "48",
+        decade: "40"
+    },
+    {
+        text: "In 1948, NASCAR held its first race for modified stock cars where?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Daytona Beach",
+        decade: "40"
+    },
+    {
+        text: "In 1942, the minimum draft age is lowered from 21 years old to what in the United States?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "18",
+        decade: "40"
+    },
+    {
+        text: "In 1943, what building, considered to be the world’s largest office building, was completed?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "The Pentagon",
+        decade: "40"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia802301.us.archive.org/32/items/lp_take-the-a-train_duke-ellington/disc1/01.01.%20Take%20The%20%22A%22%20Train_sample.mp3",
+        answer: "Take the 'A' Train by Duke Ellington",
+        decade: "40"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://www.biography.com/.image/t_share/MTgwMTU2NjQxNTU4MTQ0MzQ0/gettyimages-517324714.jpg",
+        audio: "",
+        answer: "American Professional Baseball player Babe Ruth",
+        decade: "40"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://allthatsinteresting.com/wordpress/wp-content/uploads/2012/08/elvis-1950s.jpg",
+        audio: "",
+        answer: "Pop Culture Icon Elvis Presley",
+        decade: "50"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia803104.us.archive.org/9/items/cd_heartbreak-hotel_elvis-presley/disc1/01.%20Elvis%20Presley%20-%20Heartbreak%20Hotel_sample.mp3",
+        answer: "Heartbreak Hotel",
+        decade: "50"
+    },
+    {
+        text: "34th President of the United States",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Dwight D Eisenhower",
+        decade: "50"
+    },
+    {
+        text: "Which '50s film icon died young?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "James Dean",
+        decade: "50"
+    },
+    {
+        text: "Name of an icon of '50s Western films?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "John Wayne",
+        decade: "50"
+    },
+    {
+        text: "During the Red Scare, the U.S. government attempted to uncover:",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Communists",
+        decade: "50"
+    },
+    {
+        text: "In 1955, who became famous for not giving up their seat to a white person on a Montgomery, Alabama bus?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Rosa Parks",
+        decade: "50"
+    },
+    {
+        text: "Popular sex icon of the 1950s",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Marilyn Monroe",
+        decade: "50"
+    },
+    {
+        text: "What genre dominated music in the late '50s?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Rock & Roll",
+        decade: "50"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia804509.us.archive.org/10/items/lp_autumn-leaves-the-best-of-roger-williams_roger-williams/disc1/01.01.%20Autumn%20Leaves_sample.mp3",
+        answer: "Autumn Leaves by Roger Williams",
+        decade: "50"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia601608.us.archive.org/2/items/THE_ROLLING_STONES_I_Cant_Get_No_Satisfaction_1965/THE_ROLLING_STONES_I_Cant_Get_No_Satisfaction_1965.mp4",
+        answer: "(I Can't Get No) Satisfaction",
+        decade: "60"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://static.stacker.com/s3fs-public/styles/slide_desktop/s3/Cropped60sImagesFlintstonesPMZ2.png",
+        audio: "",
+        answer: "The Flintstones",
+        decade: "60"
+    },
+    {
+        text: "Massive peaceful protest in front of the Lincoln Memorial, protesting racial inequality",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "March on Washington",
+        decade: "60"
+    },
+    {
+        text: "Civil Rights Activist killed in 1965",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Malcolm X",
+        decade: "60"
+    },
+    {
+        text: "What milestone did NASA reach in 1969?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Man on the Moon",
+        decade: "60"
+    },
+    {
+        text: "Which president was assassinated in 1963?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "John F. Kennedy",
+        decade: "60"
+    },
+    {
+        text: "Who was the first televised presidential debate between?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Nixon and Kennedy",
+        decade: "60"
+    },
+    {
+        text: "What country was targeted in the Bay of Pigs Invasion?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Cuba",
+        decade: "60"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia600602.us.archive.org/30/items/THE_BEACH_BOYS_I_Get_Around_1964/THE_BEACH_BOYS_I_Get_Around_1964.mp4",
+        answer: "I Get Around by The Beach Boys",
+        decade: "60"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://media.gettyimages.com/photos/woman-running-through-the-mud-at-the-woodstock-music-festival-new-us-picture-id1161372043?s=2048x2048",
+        audio: "",
+        answer: "Woodstock Music Festival",
+        decade: "60"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia903100.us.archive.org/7/items/queen-bohemian-rhapsody_202106/QUEEN%20Bohemian%20Rhapsody.mp4",
+        answer: "Bohemian Rhapsody",
+        decade: "70"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/head-coach-don-shula-and-nick-bouniconti-of-the-miami-news-photo-1573860198.jpg?crop=1xw:1xh;center,top&resize=980:",
+        audio: "",
+        answer: "Miami Dolphins win Super Bowl VII",
+        decade: "70"
+    },
+    {
+        text: "Major political scandal leading to President Nixon's resignation",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Watergate Scandal",
+        decade: "70"
+    },
+    {
+        text: "The line 'Houston, we've had a problem' was famously uttered aboard what troubled spaceflight?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Apollo 13",
+        decade: "70"
+    },
+    {
+        text: "What show set a record when it had nearly 100 million people tune in for its finale on January 30th, 1977?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Roots",
+        decade: "70"
+    },
+    {
+        text: "In 1972, what car surpassed Ford's iconic Model T to become the most highly produced car in the world?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Volkswagen Beetle",
+        decade: "70"
+    },
+    {
+        text: "In 1972, what car surpassed Ford's iconic Model T to become the most highly produced car in the world?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Volkswagen Beetle",
+        decade: "70"
+    },
+    {
+        text: "Which horse won the 1973 Kentucky Derby, Preakness, and Belmont Stakes becoming the 9th horse ever to take home the Triple Crown?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Secretariat",
+        decade: "70"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia601305.us.archive.org/35/items/cd_billboard-top-rb-hits---1970_various_201504/90-0168/Various%20%281989%29%20Billboard%20Top%20R%26B%20Hits%20-%201970/04%20-%20Diana%20Ross%20-%20Ain%27t%20No%20Mountain%20High%20Enough_sample.mp3",
+        answer: "Ain't No Mountain High Enough by Diana Ross",
+        decade: "70"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/british-actor-robert-shaw-on-the-set-of-jaws-directed-by-news-photo-1573929653.jpg?crop=1xw:1xh;center,top&resize=980:*",
+        audio: "",
+        answer: "Popular film 'Jaws'",
+        decade: "70"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia803403.us.archive.org/31/items/michael-jackson-billie-jean/MICHAEL%20JACKSON%20%20Billie%20Jean.mp4",
+        answer: "Billie Jean",
+        decade: "80"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://www.rd.com/wp-content/uploads/2018/09/shutterstock_1599997a-scaled.jpg?resize=1000,1536",
+        audio: "",
+        answer: "Michael Jackson",
+        decade: "80"
+    },
+    {
+        text: "First Woman elected US Supreme Court Justice",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Sandra Day O'Connor",
+        decade: "80"
+    },
+    {
+        text: "Microsoft's Windows operating system, Windows 1.0, was released in what year?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "1985",
+        decade: "80"
+    },
+    {
+        text: "In 1986, what American space shuttle disintegrated after launch killing all aboard?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Challenger",
+        decade: "80"
+    },
+    {
+        text: "Which 1980s game show featured the 'whammy?'",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "Press Your Luck",
+        decade: "80"
+    },
+    {
+        text: "What Las Vegas hotel caught fire in 1980, killing 85 and injuring roughly 700 people?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "MGM Grande",
+        decade: "80"
+    },
+    {
+        text: "What Las Vegas hotel caught fire in 1980, killing 85 and injuring roughly 700 people?",
+        mediaType: "None",
+        image: "",
+        audio: "",
+        answer: "$1.12",
+        decade: "80"
+    },
+    {
+        text: "What does this photo mean to you?",
+        mediaType: "Image",
+        image: "https://media.gettyimages.com/photos/team-usa-celebrates-their-43-victory-over-the-soviet-union-in-the-picture-id1392799?s=2048x2048",
+        audio: "",
+        answer: "Team USA Victory over Soviet Union in the 'Miracle on Ice'",
+        decade: "80"
+    },
+    {
+        text: "Name this song!",
+        mediaType: "Audio",
+        image: "",
+        audio: "https://ia801604.us.archive.org/6/items/Queen_Crazy_Little_Thing_Called_Love/Queen_Crazy_Little_Thing_Called_Love.mp4",
+        answer: "Crazy Little Thing Called Love by Queen",
+        decade: "80"
+    },
+]
